41c454bb0209d35d9b3f27f4ceecdbde89f87395
Francis Langlois <francis.langlois@gmail.com>
2016-11-21 00:10:52 -0500

    refactor npc (#86)

--- js/app.js
+++ js/app.js
@@ -1,29 +1,3 @@
-// ------ NPCs -------
-// Other characters in the game, can be friendly or hostile
-var NPC = function ( x, y ) {
-	// Variables applied to each of our instances go here
-	this.sprite = wZ;
-	this.x= x;
-	this.y = y;
-};
-
-// Update the NPC's position, required method for game
-// Parameter: dt, a time delta between ticks
-NPC.prototype.update = function () {
-};
-
-// Draw the NPC on the screen
-NPC.prototype.render = function () {
-	ctx.drawImage( Resources.get( this.sprite ), this.x, this.y );
-};
-
-// Class of NPC to heal your monsters, give it location and which
-// which level to be displayed on
-var HealerNPC = function ( x, y, level ) {
-	NPC.call( this,x, y );
-	this.level = level;
-};
-
 HealerNPC.prototype = Object.create( NPC.prototype );
 HealerNPC.prototype.constructor = HealerNPC;
 HealerNPC.prototype.level = 0;
