90d72c1ecbd684d5b8ee64f40da05e17ab28d856
Francis Langlois <francis.langlois@gmail.com>
2017-01-09 15:39:16 -0500

    refactored controls for player to select current level controls

--- scripts/components/player/controls.js
+++ scripts/components/player/controls.js
@@ -1,61 +1,24 @@
-import allLevels from './../levels/all-levels';
+// import allLevels from './../levels/all-levels';
 // if ( currentLevel != 'battleLevel') {
 // Handles the player input, called on every key press
 export default function ( key ) {
 
-  this.render();
+    this.render();
+    currentLevel.controls(key, player)
+    this.locationSetter( key );
 
-  switch ( currentLevel ) {
-    case allLevels.startScreen:
-        allLevels.startScreen.controls( key, player );
-        this.locationSetter( key );
-    break;
-
-    case allLevels.characterSelect:
-        allLevels.characterSelect.controls( key, player );
-        this.locationSetter( key );
-    break;
-
-    case allLevels.monsterSelect:
-        monsterSelectControls( key, player );
-        this.locationSetter( key );
-    break;
-
-    case allLevels.battleLevel:
-        battleControls( key, player );
-        this.locationSetter( key );
-    break;
-
-    case allLevels.firstLevel:
-    case allLevels.fireLevel:
-    case allLevels.waterLevel:
-        worldControls( key, player );
-        this.locationSetter( key );
-        if ( key === 'left' || key === 'up' || key === 'right' || key === 'down' ) {
-            battleEvent();
-        };
-        if ( key === 'space' ) {
-            npcControls( player );
-        }
-    break;
-
-    case allLevels.mainMenu:
-        mainMenuControls( key, player );
-        this.locationSetter( key );
-    break;
-
-    case allLevels.inventoryMenu:
-        inventoryMenuControls( key, player );
-        this.locationSetter( key );
-    break;
-
-    case allLevels.monsterInventory:
-        monsterInventoryMenuControls( key, player );
-        this.locationSetter( key );
-    break;
-
-    case allLevels.gameOver:
-        gameOverControls( key, player );
-    break;
-  }
+  // switch ( currentLevel ) {
+  //   case allLevels.firstLevel:
+  //   case allLevels.fireLevel:
+  //   case allLevels.waterLevel:
+  //       worldControls( key, player );
+  //       this.locationSetter( key );
+  //       if ( key === 'left' || key === 'up' || key === 'right' || key === 'down' ) {
+  //           battleEvent();
+  //       };
+  //       if ( key === 'space' ) {
+  //           npcControls( player );
+  //       }
+  //   break;
+  // }
 };
