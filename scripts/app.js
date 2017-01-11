6fafcc9f6e6c5c9ff5207c7b30440bcd37d0e602
Francis Langlois <francis.langlois@gmail.com>
2017-01-10 21:23:38 -0500

    items are now properly generated

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,11 @@
 // import startScreen from './components/levels/start-screen/start-screen';
 import {Player} from './components/player/player';
-import allLevels from './components/levels/all-levels'
+import allLevels from './components/levels/all-levels';
+import allItems from './components/items/items';
 
 var currentLevel = window.currentLevel = allLevels.startScreen;
 var player = window.player = new Player();
+var items = window.items = allItems;
 
 player.inventory.push( items.net );
 player.inventory.push( items.potion );
--- scripts/components/items/net.js
+++ scripts/components/items/net.js
@@ -1,37 +1,36 @@
 export default {
   name:'Net',
-    singleUse: false,
-    func: function () {
-      var enemy = battle.enemy;
+  singleUse: false,
+  func: function () {
+    var enemy = battle.enemy;
 
-      if ( currentLevel === levels.battleLevel ) {
-        var hpPercent = enemy.currentHp / enemy.hp;
+    if ( currentLevel === levels.battleLevel ) {
+      var hpPercent = enemy.currentHp / enemy.hp;
 
-        var catchMonster = function ( probability ) {
-          if ( rng() <= probability ) {
-            if ( player.monsterInventory[0].name === 'PlayerMon' ) {
-              battle.playerBattleMonster.player = 0;
-              player.monsterInventory.pop();
-            }
-
-            enemy.controller = player;
-            player.monsterInventory.push( enemy );
-            items.net.battleState = 'caughtMonster';
-          } else {
-            items.net.battleState = 'failedCatch';
+      var catchMonster = function ( probability ) {
+        if ( rng() <= probability ) {
+          if ( player.monsterInventory[0].name === 'PlayerMon' ) {
+            battle.playerBattleMonster.player = 0;
+            player.monsterInventory.pop();
           }
-        };
 
-        // Probability of successfully catching a monster increases with decreasing monster health %
-        if ( hpPercent >= 0.9 ) {
-          catchMonster( 1 );
-        } else if ( hpPercent >= 0.6 ) {
-          catchMonster( 0.5 );
-        } else if ( hpPercent >= 0.3 ) {
-          catchMonster( 0.85 );
+          enemy.controller = player;
+          player.monsterInventory.push( enemy );
+          items.net.battleState = 'caughtMonster';
         } else {
-          catchMonster( 1 );
+          items.net.battleState = 'failedCatch';
         }
+      };
+
+      // Probability of successfully catching a monster increases with decreasing monster health %
+      if ( hpPercent >= 0.9 ) {
+        catchMonster( 1 );
+      } else if ( hpPercent >= 0.6 ) {
+        catchMonster( 0.5 );
+      } else if ( hpPercent >= 0.3 ) {
+        catchMonster( 0.85 );
+      } else {
+        catchMonster( 1 );
       }
     }
   }
