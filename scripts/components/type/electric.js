0fa87403de4a7f4887955574559c5292ce189fcb
Francis Langlois <francis.langlois@gmail.com>
2017-01-16 18:19:10 -0500

    elemental type cleaned up

--- scripts/components/type/electric.js
+++
@@ -1,7 +0,0 @@
-var ElectricType = function ( lvl ) {
-  Monster.call( this, lvl );
-};
-
-ElectricType.prototype = Object.create( Monster.prototype );
-ElectricType.prototype.constructor = ElectricType;
-ElectricType.prototype.type = 'electric';
--- scripts/components/type/ghost.js
+++
@@ -1,7 +0,0 @@
-var GhostType = function ( lvl ) {
-  Monster.call( this, lvl );
-};
-
-GhostType.prototype = Object.create( Monster.prototype );
-GhostType.prototype.constructor = GhostType;
-GhostType.prototype.type = 'ghost';
