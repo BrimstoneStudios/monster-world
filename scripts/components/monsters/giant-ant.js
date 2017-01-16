4703d358645896e5d39b70d41f87b63fa0970d19
Francis Langlois <francis.langlois@gmail.com>
2017-01-16 15:11:57 -0500

    giant ant linter now passes

--- scripts/components/monsters/giant-ant.js
+++ scripts/components/monsters/giant-ant.js
@@ -9,23 +9,24 @@ const spAttackMultiplier = 2;
 const spDefenseMultiplier = 2;
 const speedMultiplier = 2;
 
-export class  GiantAnt extends Monster {
-  constructor( lvl ) {
-    super( lvl, hpMultiplier, attackMultiplier, defenseMultiplier, spAttackMultiplier, spDefenseMultiplier, speedMultiplier );
+export class GiantAnt extends Monster {
+  constructor(lvl) {
+    super(
+      lvl,
+      hpMultiplier,
+      attackMultiplier,
+      defenseMultiplier,
+      spAttackMultiplier,
+      spDefenseMultiplier,
+      speedMultiplier
+    );
+
     this.abilities = [
       abilities.bite,
       abilities.growl,
-     abilities.razorLeaf,
+      abilities.razorLeaf,
     ];
-    this.items = [
-      {
-       item: items.net,
-       dropProbablity: 1
-      }, {
-       item: items.potion,
-       dropProbablity: 1
-      },
-    ];
+
     this.name = 'Giant Ant';
     this.sprite = 'images/monsters/giant-ant.jpg';
     this.type = types.grass;
