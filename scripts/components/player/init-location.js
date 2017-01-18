300a553fcae0e4ce85bd18bf449849f80b977e49
Francis Langlois <francis.langlois@gmail.com>
2017-01-18 16:13:19 -0500

    player component cleaned up (#107)

--- scripts/components/player/init-location.js
+++
@@ -1,6 +0,0 @@
-export default function () {
-  if (typeof currentMap.initLocation !== 'undefined') {
-    this.location.x = currentMap.initLocation.x;
-    this.location.y = currentMap.initLocation.y;
-  }
-}
--- scripts/components/player/view.js
+++
@@ -1,8 +0,0 @@
-Player.prototype.initBorderTransitionLocation = function (coordinates) {
-  if (typeof coordinates.x !== 'undefined') {
-    this.location.x = coordinates.x;
-  }
-  if (typeof coordinates.y !== 'undefined') {
-    this.location.y = coordinates.y;
-  }
-};
