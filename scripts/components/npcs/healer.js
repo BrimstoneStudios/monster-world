4c085693c8a4f89c6b8cee7529ed3375ab3bf82b
Francis Langlois <francis.langlois@gmail.com>
2017-01-18 17:00:17 -0500

    npcs cleaned up and linted

--- scripts/components/npcs/healer.js
+++ scripts/components/npcs/healer.js
@@ -1,11 +1,16 @@
 import NPC from './npc';
 
 // NPC that heals all of your monsters
+function initNPCAction() {
+  for (let i = 0; i < player.monsterInventory.length; i++) {
+    player.monsterInventory[i].currentHp = player.monsterInventory[i].hp;
+  }
+}
+
 class HealerNPC extends NPC {
-  initAction() {
-    for (let i = 0; i < player.monsterInventory.length; i++) {
-      player.monsterInventory[i].currentHp = player.monsterInventory[i].hp;
-    }
+  constructor(name, sprite, x, y) {
+    super(name, sprite, x, y);
+    this.initNPCAction = initNPCAction
   }
 }
 
