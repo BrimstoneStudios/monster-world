221df1474462d095fa0ebb9b215f4418bfcd9ebb
Francis Langlois <francis.langlois@gmail.com>
2017-01-18 16:12:20 -0500

    player component cleaned up

--- scripts/components/player/controls.js
+++
@@ -1,7 +0,0 @@
-// if ( currentMap != 'battleMap') {
-// Handles the player input, called on every key press
-export default function (key) {
-  this.render();
-  currentMap.controls(key, player)
-  this.locationSetter(key);
-}
