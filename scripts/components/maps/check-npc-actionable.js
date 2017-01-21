1647bba67bfef921c5a7366f3b122315543d789d
Francis Langlois <francis.langlois@gmail.com>
2017-01-21 15:43:18 -0500

    ran lint on npc actionable check

--- scripts/components/maps/check-npc-actionable.js
+++ scripts/components/maps/check-npc-actionable.js
@@ -1,7 +1,9 @@
 export default function () {
   if (typeof currentMap.npcs !== 'undefined') {
     for (let i = 0; i < currentMap.npcs.length; i++) {
-      if (player.location.x === currentMap.npcs[i].location.x && player.location.y === currentMap.npcs[i].location.y) {
+      const npcLocation = currentMap.npcs[i].location;
+
+      if (player.location.x === npcLocation.x && player.location.y === npcLocation.y) {
         currentMap.npcs[i].initNPCAction();
       }
     }
