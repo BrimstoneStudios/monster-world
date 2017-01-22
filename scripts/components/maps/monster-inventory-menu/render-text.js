805f961638355e0cf152bb25d44e5fb6dd2f6e8a
Francis Langlois <francis.langlois@gmail.com>
2017-01-21 19:03:21 -0500

    monster inventory render text linted

--- scripts/components/maps/monster-inventory-menu/render-text.js
+++ scripts/components/maps/monster-inventory-menu/render-text.js
@@ -1,29 +1,32 @@
 export default function () {
-  for (let i = 0, j = 0; i < player.monsterInventory.length; i++, j += currentMap.movement.distance.y) {
+  const inventory = player.monsterInventory;
+
+  for (let i = 0, j = 0; i < inventory.length; i++, j += currentMap.movement.distance.y) {
     ctx.font = '30px Arial';
-    ctx.drawImage(Resources.get(player.monsterInventory[i].sprite), 85, 50 + j);
-    ctx.fillText(player.monsterInventory[i].name, 155, 85 + j);
+    ctx.drawImage(Resources.get(inventory[i].sprite), 85, 50 + j);
+    ctx.fillText(inventory[i].name, 155, 85 + j);
 
     if (currentMap.displayStats) {
-      let monster = currentMap.displayMonsterIndex;
+      const monsterIndex = currentMap.displayMonsterIndex;
+      const monster = inventory[monsterIndex];
 
       ctx.font = '25px Arial';
       ctx.fillText('Level:', 430, 65);
-      ctx.fillText(player.monsterInventory[monster].level, 620, 65);
+      ctx.fillText(monster.level, 620, 65);
       ctx.fillText('HP:', 430, 105);
-      ctx.fillText(player.monsterInventory[monster].hp, 620, 105);
+      ctx.fillText(monster.hp, 620, 105);
       ctx.fillText('Attack:', 430, 145);
-      ctx.fillText(player.monsterInventory[monster].attack, 620, 145);
+      ctx.fillText(monster.attack, 620, 145);
       ctx.fillText('Defense:', 430, 185);
-      ctx.fillText(player.monsterInventory[monster].defense, 620, 185);
+      ctx.fillText(monster.defense, 620, 185);
       ctx.fillText('Sp Attack:', 430, 225);
-      ctx.fillText(player.monsterInventory[monster].spAttack, 620, 225);
+      ctx.fillText(monster.spAttack, 620, 225);
       ctx.fillText('Sp Defense:', 430, 265);
-      ctx.fillText(player.monsterInventory[monster].spDefense, 620, 265);
+      ctx.fillText(monster.spDefense, 620, 265);
       ctx.fillText('Speed:', 430, 305);
-      ctx.fillText(player.monsterInventory[monster].speed, 620, 305);
+      ctx.fillText(monster.speed, 620, 305);
       ctx.fillText('Type:', 430, 345);
-      ctx.fillText(player.monsterInventory[monster].type, 620, 345);
+      ctx.fillText(monster.type, 620, 345);
     }
   }
 }
