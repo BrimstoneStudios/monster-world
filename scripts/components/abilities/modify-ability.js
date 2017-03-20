ac6492091992aae4e1bd0f892a85cbd337f250bd
Francis Langlois <francis.langlois@gmail.com>
2017-03-20 16:52:06 -0400

    modify-ability linted

--- scripts/components/abilities/modify-ability.js
+++ scripts/components/abilities/modify-ability.js
@@ -1,17 +1,17 @@
 import rng from './../rng';
 
 export default function (ability, attacker, defender) {
-  var target;
+  const attributeAffected = ability.attribute;
+  let target;
 
   ability.targetSelf ? target = attacker : target = defender;
 
-  var attributeAffected = ability.attribute;
 
   if (attributeAffected === 'condition') {
     if (rng() <= ability.modifier) {
       target[attributeAffected] = ability.conditionApplied;
     }
   } else {
-    target[attributeAffected] = target[attributeAffected] * ability.modifier;
+    target[attributeAffected] *= ability.modifier;
   }
 }
