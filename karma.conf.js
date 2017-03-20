b441143cfce576c58865ab71147beb9829863fd6
Francis Langlois <francis.langlois@gmail.com>
2017-03-20 18:20:47 -0400

    karma coverage still doesn't work

--- karma.conf.js
+++ karma.conf.js
@@ -29,15 +29,15 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'scripts/app.js': ['webpack'],
-      'tests/*-spec.js': ['webpack']
+        'scripts/app.js': ['webpack', 'coverage'],
+        'tests/*-spec.js': ['webpack']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
@@ -69,6 +69,10 @@ module.exports = function(config) {
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity,
-    webpack: webpackConfig
+    webpack: webpackConfig,
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    }
   })
 }
