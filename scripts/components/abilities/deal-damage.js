10acbd0eb4f0c4a6782d8ecea896ade16628598e
Francis Langlois <francis.langlois@gmail.com>
2017-03-20 16:54:31 -0400

    deal-damage linted

--- scripts/components/abilities/deal-damage.js
+++ scripts/components/abilities/deal-damage.js
@@ -1,12 +1,15 @@
 export default function (attacker, ability, damageModifier, defender) {
   // Still needs accuracy modifier
-  // Physical attacks use the attack and defense attributes
-  var damage;
+  let damage;
+  const power = ability.power;
 
+  // Physical attacks use the attack and defense attributes
   if (ability.category === 'physical') {
-    damage = ability.power * (attacker.attack * 1.5) * 0.04 / defender.defense * damageModifier;
-  } else if (ability.category === 'special') { // Special attacks use the spAttack and spDefense attributes
-    damage = ability.power * (attacker.spAttack * 1.5) * 0.04 / defender.spDefense * damageModifier;
+    damage = power * (attacker.attack * 1.5) * 0.04 / defender.defense * damageModifier;
+
+  // Special attacks use the spAttack and spDefense attributes
+  } else if (ability.category === 'special') {
+    damage = power * (attacker.spAttack * 1.5) * 0.04 / defender.spDefense * damageModifier;
   }
 
   if (typeof damage !== 'undefined') {
