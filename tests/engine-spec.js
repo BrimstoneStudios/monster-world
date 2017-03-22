c52dc5e31a89d98b835d0694aa26cab116bdc660
Francis Langlois <francis.langlois@gmail.com>
2017-03-22 16:18:14 -0400

    engine specs (#115)

---
+++ tests/engine-spec.js
@@ -0,0 +1,19 @@
+import engine from '../scripts/components/engine/engine';
+
+describe('engine: Every new engine object', function () {
+  beforeEach(function () {
+    this.mockEngine = engine;
+  })
+  it('should contain the imageCache property', function () {
+    expect(this.mockEngine.imageCache).toBeDefined();
+  })
+  it('should have a imageCache property that\'s an object', function () {
+    expect(typeof this.mockEngine.imageCache).toBe('object')
+  })
+  it('should contain the start property', function () {
+    expect(this.mockEngine.start).toBeDefined();
+  })
+  it('should have a start property that\'s a function', function () {
+    expect(typeof this.mockEngine.start).toBe('function')
+  })
+});
